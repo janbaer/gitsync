4c02e9b552d0788460cdb296e8e2b70321f0a010
Jan Baer <jan@janbaer.de>
2013-07-20 10:34:02 +0200

    Some optimizations and better error handling

--- gitsync.js
+++ gitsync.js
@@ -1,56 +1,59 @@
 'use strict';
 
 var fs = require('fs'),
-    path = require('path'),
-    exec = require('child_process').exec,
-    util = require('util'),
-    os = require('os'),
-    colors = require('colors'),
-    _ = require('underscore');
+  path = require('path'),
+  exec = require('child_process').exec,
+  util = require('util'),
+  os = require('os'),
+  colors = require('colors'),
+  _ = require('underscore');
+
+var readRepositories = function(fileName) {
+  try {
+    var json = fs.readFileSync(fileName);
+    return JSON.parse(json);
+  } catch (error) {
+    console.error(util.format('Error on reading config file %s - %s', fileName, error));
+    return null;
+  }
+};
 
 var gitcommand = '/usr/local/git/bin/git';
 
-if (os.platform() === 'darwin') {
-  gitcommand = '/usr/local/git/bin/git';
-} else {
+if (os.platform() !== 'darwin') {
   gitcommand = process.env.GIT_BIN;
 }
 
 var fileName = path.join(__dirname, '/repositories.json');
 
-fs.readFile(fileName, 'utf8', function (error, data) {
-  if (error) {
-    console.error(util.format('Error on reading config file %s - %s', fileName, error));
+var repositories = readRepositories(fileName);
+if (repositories === null) {
+  return;
+}
+
+console.log('Updating all defined repositories...'.green);
+console.log('------------------------------------'.yellow);
+
+_.each(repositories, function(repository) {
+  if (!fs.existsSync(repository.path)) {
+    console.log(util.format('The directory for the repository "%s" not exists: %s', repository.name, repository.path).red);
+    return;
   }
 
-  var repositories = JSON.parse(data);
+  var options = {
+    cwd: repository.path,
+    env: null
+  }
 
-  console.log('Updating all defined repositories...'.green);
-  console.log('------------------------------------'.yellow);
+  exec(gitcommand + ' pull', options, function(error, stdout) {
+    var message;
 
-  _.each(repositories, function (repository) {
-    if (!fs.existsSync(repository.path)) {
-      console.log(util.format('The directory for the repository "%s" not exists: %s', repository.name, repository.path).red);
-      return;
+    if (error !== null) {
+      message = util.format('Error on updating repository %s - %s', repository.name, error).red;
+    } else {
+      message = util.format('Updating repository "%s" - %s', repository.name, stdout).green;
     }
-
-    var options = {
-      cwd: repository.path,
-      env: null
-    }
-
-    exec(gitcommand + ' pull', options, function (error, stdout) {
-      var message;
-
-      if (error !== null) {
-        message = util.format('Error on updating repository %s - %s', repository.name, error).red;
-      } else {
-        message = util.format('Updating repository "%s" - %s', repository.name, stdout).green;
-      }
-      console.log(message);
-    });
-
+    console.log(message);
   });
-
 });
 
