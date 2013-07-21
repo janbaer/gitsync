508d64f90b0cf2e3e43589a499c8bd7b01caf1fd
Jan Baer <jan@janbaer.de>
2013-07-21 14:07:00 +0200

    gitsync uses now the repositoryReader

--- gitsync.js
+++ gitsync.js
@@ -8,15 +8,7 @@ var fs = require('fs'),
   colors = require('colors'),
   _ = require('underscore');
 
-var readRepositories = function(fileName) {
-  try {
-    var json = fs.readFileSync(fileName);
-    return JSON.parse(json);
-  } catch (error) {
-    console.error(util.format('Error on reading config file %s - %s', fileName, error));
-    return null;
-  }
-};
+var reader = require('./repositoryReader');
 
 var gitcommand = '/usr/local/git/bin/git';
 
@@ -26,34 +18,36 @@ if (os.platform() !== 'darwin') {
 
 var fileName = path.join(__dirname, '/repositories.json');
 
-var repositories = readRepositories(fileName);
-if (repositories === null) {
-  return;
-}
+reader.readFile(fileName)
+      .then(function (repositories) {
+        console.log('Updating all defined repositories...'.green);
+        console.log('------------------------------------'.yellow);
 
-console.log('Updating all defined repositories...'.green);
-console.log('------------------------------------'.yellow);
+        _.each(repositories, function(repository) {
+          if (!fs.existsSync(repository.path)) {
+            console.log(util.format('The directory for the repository "%s" not exists: %s', repository.name, repository.path).red);
+            return;
+          }
 
-_.each(repositories, function(repository) {
-  if (!fs.existsSync(repository.path)) {
-    console.log(util.format('The directory for the repository "%s" not exists: %s', repository.name, repository.path).red);
-    return;
-  }
+          var options = {
+            cwd: repository.path,
+            env: null
+          }
 
-  var options = {
-    cwd: repository.path,
-    env: null
-  }
+          exec(gitcommand + ' pull', options, function(error, stdout) {
+            var message;
 
-  exec(gitcommand + ' pull', options, function(error, stdout) {
-    var message;
+            if (error !== null) {
+              message = util.format('Error on updating repository %s - %s', repository.name, error).red;
+            } else {
+              message = util.format('Updating repository "%s" - %s', repository.name, stdout).green;
+            }
+            console.log(message);
+          });
+        });
+      })
+      .catch(function (error) {
+        console.error(util.format('error while reading repositories definition: $s', error));
+      })
 
-    if (error !== null) {
-      message = util.format('Error on updating repository %s - %s', repository.name, error).red;
-    } else {
-      message = util.format('Updating repository "%s" - %s', repository.name, stdout).green;
-    }
-    console.log(message);
-  });
-});
 
